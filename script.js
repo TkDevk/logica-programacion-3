95f58dde7657d23a04eb0b370f414d358840a3b4
TkDevk <duvanuriel@hotmail.com>
2023-10-17 22:54:44 -0500

    completed

---
+++ script.js
@@ -0,0 +1,10 @@
+function calcularFactorial()
+{const inputNumber = parseFloat(document.getElementById('num').value);
+let resultado=1;
+for(let i=1; i<inputNumber+1;i++){
+
+     resultado= resultado*i;
+    }
+    const resultadoMostrar = document.getElementById('mostrarResultado');
+    resultadoMostrar.textContent = `El factorial de ${inputNumber} es:  ${resultado}`
+}
